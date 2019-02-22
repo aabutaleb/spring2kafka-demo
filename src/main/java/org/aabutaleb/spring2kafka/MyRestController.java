c7b54c2dba7fd856f7327559b31a69e83e98a7c3
Amin Abu-Taleb <abta.amin@gmail.com>
2019-02-22 15:45:19 +0000

    Working

---
+++ src/main/java/org/aabutaleb/spring2kafka/MyRestController.java
@@ -0,0 +1,23 @@
+package org.aabutaleb.spring2kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class MyRestController {
+    private final ProducerService producer;
+
+    @Autowired
+    MyRestController(ProducerService producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
